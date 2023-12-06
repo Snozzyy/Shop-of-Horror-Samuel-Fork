83870177ab3c3854a3403f2794cf9d18933347be
Snozzyy <oliverschuller@hotmail.com>
2023-12-06 22:53:03 +0100

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,11 +1,9 @@
 public class Customer {
     private double totalCost;
-    private int unitsBought;
 
     public Customer(){
         /*When a customer starts the session cost and bought units should be zero*/
         totalCost = 0;
-        unitsBought = 0;
     }
 
     public void addToTotalCost(double totalCost) {
